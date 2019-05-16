d0de1d40d4efdd14936302863964dcbfd8f537aa
吴继 <wuji@MacBook-Pro.local>
2019-05-16 10:40:57 +0800

    添加一周健康数据

--- addons/ewei_shopv2/core/web/hospital/healthBracelet.php
+++ addons/ewei_shopv2/core/web/hospital/healthBracelet.php
@@ -16,10 +16,10 @@ class HealthBracelet_EweiShopV2Page extends WebPage
 	public function main()
 	{
 		global $_GPC;
+		$mobile = $_GPC['mobile'];
+		if (empty($mobile)) exit(show_json(0, '手机号码错误'));
+		$imei = pdo_getcolumn('member_wristband', ['mobile' => $mobile], 'imei');
 		if (is_ajax()) {
-			$mobile = $_GPC['mobile'];
-			if (empty($mobile)) exit(show_json(0, '手机号码错误'));
-			$imei = pdo_getcolumn('member_wristband', ['mobile' => $mobile], 'imei');
 			$info = json_decode($this->requestApi($imei, 'http://api.jiai.pro:8080/jiai/location'), true);
 			if ($info['code'] != '0000') {
 				exit(show_json(0, $info['message']));
@@ -28,18 +28,31 @@ class HealthBracelet_EweiShopV2Page extends WebPage
 			exit(show_json(1, $res));
 		}
 		// 会员健康列表
-		$list = pdo_fetchall("SELECT a.id,a.openid,c.realname,b.heartRate,b.dbp,b.sdp,b.bloodSugar,b.oxygen
-									FROM `ims_member_wristband` a
-									LEFT JOIN `ims_wristband_health` b
-									ON a.imei = b.imei
-									LEFT JOIN `ims_ewei_shop_member` c
-									ON a.openid = c.openid");
+		$health = pdo_fetch("select * from `ims_wristband_health` where imei = " . $imei . " order by id desc");
+		$week_health = pdo_getall('wristband_health', ['imei' => $imei], [], '', 'id desc', [7]);
+		sort($week_health);
+		dump($week_health);
 		include $this->template('hospital/health_bracelet/index');
+	}
+
+	public function health()
+	{
+		global $_GPC;
+		$mobile = $_GPC['mobile'];
+		if (empty($mobile)) exit(show_json(0, '手机号码错误'));
+		$imei = pdo_getcolumn('member_wristband', ['mobile' => $mobile], 'imei');
+		$info = json_decode($this->requestApi($imei, 'http://api.jiai.pro:8080/jiai/bloodPressure'), true);
+		if ($info['code'] != '0000') {
+			exit(show_json(0, $info['message']));
+		}
+		$res = pdo_fetch("select * from `ims_wristband_location` where imei = ". $imei ." order by id desc");
+		exit(show_json(1, '获取成功'));
 	}
 
 	/**
 	 * 请求第三方定位接口
 	 * @param string $imei 串号
+	 * @param string $url 地址
 	 * @return bool|string
 	 */
 	public function requestApi($imei = '', $url = '')
